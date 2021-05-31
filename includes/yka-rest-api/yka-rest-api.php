cd3684f461833c47eb0745dd3a1a9425471d74ee
psychome004 <anil.stephen66@gmail.com>
2021-05-31 11:26:31 +0530

    Removed custom-endpoints

--- includes/yka-rest-api/yka-rest-api.php
+++ includes/yka-rest-api/yka-rest-api.php
@@ -2,25 +2,7 @@
 
 	require_once('yka-rest-response.php');
 
-	$controllers = array(
-		'class-yka-rest-conversations-controller.php',
-		'class-yka-rest-comments-controller.php'
-	);
-
-	foreach( $controllers as $controller ){
-		require_once('endpoints/'.$controller);
-	}
-
-
-
-
 	add_action( 'rest_api_init', function () {
-
-		$conversations_controller = new YKA_Conversations_Controller;
-	  $conversations_controller->register_routes();
-
-		$comments_controller = new YKA_Comments_Controller;
-	  $comments_controller->register_routes();
 
 		// CPT CONVERSATION
 		register_rest_field( 'conversation', 'number_of_bookmarks', array(
