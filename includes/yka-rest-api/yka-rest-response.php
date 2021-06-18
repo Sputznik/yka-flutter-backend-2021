a838bc5e8fa57a42cb1458e51d0e4d204a2e5564
psychome004 <anil.stephen66@gmail.com>
2021-06-18 13:27:03 +0530

    Remove wpautop and decode html-entities

--- includes/yka-rest-api/yka-rest-response.php
+++ includes/yka-rest-api/yka-rest-response.php
@@ -33,3 +33,31 @@ function yka_remove_extra_data($data, $post, $context) {
 }
 add_filter('rest_prepare_conversation', 'yka_remove_extra_data', 12, 3);
 add_filter('rest_prepare_yka-comment', 'yka_remove_extra_data', 12, 3);
+
+
+// REMOVES AUTO GENERATED P TAGS
+add_filter( 'the_content', function( $content ){
+	$cpts = ['conversation', 'yka-comment'];
+	if( in_array( get_post_type(), $cpts ) ) { remove_filter( 'the_content', 'wpautop' ); }
+	return $content;
+}, 0 );
+
+add_filter( 'rest_request_after_callbacks', function( $response, array $handler, \WP_REST_Request $request ) {
+	$yka_routes = array( '/wp/v2/conversation', '/wp/v2/yka-comment');
+	if( in_array( $request->get_route(), $yka_routes ) ){
+    if( ! ( $response instanceof \WP_REST_Response ) ){ return; }
+    $data = array_map( 'modify_yka_post_response', $response->get_data() );
+    $response->set_data( $data );
+  }
+  return $response;
+}, 10, 3 );
+
+function modify_yka_post_response( array $post ) {
+	$fields = array('title','content');
+	foreach( $fields as $field ){
+		if( isset( $post[$field]['rendered'] ) && $post[$field]['rendered'] ) {
+      $post[$field]['rendered'] = html_entity_decode( $post[$field]['rendered'] ); // DECODES HTML ENTITIES
+		}
+	}
+  return $post;
+}
