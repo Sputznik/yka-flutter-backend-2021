31ca5bc3bd4536000882db759fdf467ad728f58e
psychome004 <anil.stephen66@gmail.com>
2021-06-21 12:41:43 +0530

    Foreign Key constraint

--- includes/db/class-yka-db-user-preference.php
+++ includes/db/class-yka-db-user-preference.php
@@ -15,13 +15,15 @@ class YKA_DB_USER_PREFERENCE extends YKA_DB_BASE{
 		$charset_collate = $this->get_charset_collate();
 
     $users_table = $wpdb->prefix . 'users' . '(ID)';
+    $terms_table = $wpdb->prefix . 'terms' . '(term_id)';
 
 		$sql = "CREATE TABLE IF NOT EXISTS $table (
 			ID BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
       user_id BIGINT(20) UNSIGNED NOT NULL,
       category_id  BIGINT(20) UNSIGNED NOT NULL,
 			PRIMARY KEY(ID),
-      FOREIGN KEY (user_id) REFERENCES $users_table
+      FOREIGN KEY (user_id) REFERENCES $users_table,
+      FOREIGN KEY (category_id) REFERENCES $terms_table
 		) $charset_collate;";
 
 		return $this->query( $sql );
