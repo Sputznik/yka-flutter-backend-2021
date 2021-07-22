bcf23af482dc6be2c24d4a79372d7e966e8d2972
psychome004 <anil.stephen66@gmail.com>
2021-07-22 13:12:42 +0530

    Refactor

--- includes/yka-rest-api/yka-rest-api.php
+++ includes/yka-rest-api/yka-rest-api.php
@@ -7,8 +7,7 @@ $inc_files = array(
 	'class-yka-rest-yka-comment.php',
 	'class-yka-rest-user.php',
 	'class-yka-rest-custom.php',
-	'class-yka-rest-response.php',
-	'class-yka-rest-follow-users.php'
+	'class-yka-rest-response.php'
 );
 
 foreach( $inc_files as $inc_file ){
