6d3c07d3b29c25294c39477df21193a931341f35
psychome004 <anil.stephen66@gmail.com>
2021-08-18 10:46:08 +0530

    Custom filter-param for conversations endpoint

--- includes/yka-rest-api/class-yka-rest-conversation.php
+++ includes/yka-rest-api/class-yka-rest-conversation.php
@@ -3,6 +3,7 @@
 class YKA_REST_CONVERSATION extends YKA_REST_POST_BASE{
   function __construct(){
     $this->setPostType( 'conversation' );
+    add_filter( 'rest_conversation_query', array( $this, 'filterRestData' ), 10, 2 );
     parent::__construct();
   }
 
@@ -64,6 +65,23 @@ class YKA_REST_CONVERSATION extends YKA_REST_POST_BASE{
       }
 	 	);
 
+  }
+
+  // OPTION TO SHOW CONVERSATIONS BASED ON THE FOLLOWING LIST OF CURRENT USER
+  function filterRestData( $args, $request ){
+		$user_following = $request->get_param( 'following' );
+    $follow_users = array(
+      'type'    => "following",
+      'user_id' => (int) ( $user_following ) // VALUE MUST BE ALWAYS >= 1
+    );
+
+    if( $follow_users['user_id'] ){
+      $follow_users_db = YKA_DB_FOLLOW_USERS::getInstance();
+      $user_ids = $follow_users_db->getUserIDs( $follow_users['user_id'], $follow_users['type']  );
+      $args['author__in'] = $user_ids ? $user_ids : array(0); // SHOW EMPTY CONVERSATIONS LIST IF USER ID DOES NOT EXIST
+    }
+
+    return $args;
   }
 
 }
