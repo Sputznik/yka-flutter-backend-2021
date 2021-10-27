0c6368c144abe5f1727cc9e0b2edc183073317dc
psychome004 <anil.stephen66@gmail.com>
2021-10-27 12:43:39 +0530

    Updated bookmarks table

--- includes/db/class-yka-bookmarks-db.php
+++ includes/db/class-yka-bookmarks-db.php
@@ -7,25 +7,29 @@ class YKA_BOOKMARKS_DB extends YKA_DB_BASE{
     // SET TABLE SLUG
     $this->setTableSlug( 'bookmarks' );
     parent::__construct();
-    
-    $this->drop_table();
 
   }
 
-  // function create(){
-  //
-	// 	$table = $this->getTable();
-	// 	$charset_collate = $this->get_charset_collate();
-  //
-	// 	$sql = "CREATE TABLE IF NOT EXISTS $table (
-	// 		ID BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  //     user_id BIGINT(20),
-  //     post_id BIGINT(20),
-	// 		PRIMARY KEY(ID)
-	// 	) $charset_collate;";
-  //
-	// 	return $this->query( $sql );
-	// }
+  function create(){
+    global $wpdb;
+		$table = $this->getTable();
+		$charset_collate = $this->get_charset_collate();
+
+    $users_table = $wpdb->prefix . 'users' . '(ID)';
+    $posts_table = $wpdb->prefix . 'posts' . '(ID)';
+
+		$sql = "CREATE TABLE IF NOT EXISTS $table (
+			ID BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+      user_id BIGINT(20) UNSIGNED NOT NULL,
+      post_id BIGINT(20) UNSIGNED NOT NULL,
+      created_on DATETIME DEFAULT CURRENT_TIMESTAMP,
+			PRIMARY KEY(ID),
+      FOREIGN KEY (user_id) REFERENCES $users_table,
+      FOREIGN KEY (post_id) REFERENCES $posts_table
+		) $charset_collate;";
+
+		return $this->query( $sql );
+	}
 
 }
 
