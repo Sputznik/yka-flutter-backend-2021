1731896b5633b185247de3387eaf1347f76ba5fd
psychome004 <anil.stephen66@gmail.com>
2021-11-30 12:20:35 +0530

    Endpoint for user password validation

--- includes/yka-rest-api/class-yka-rest-password-reset.php
+++ includes/yka-rest-api/class-yka-rest-password-reset.php
@@ -6,13 +6,23 @@ class YKA_REST_PASSWORD_RESET extends YKA_REST_BASE{
     add_action( 'rest_api_init', array( $this, 'reset_user_password' ) );
   }
 
-  /* USER PASSWORD RESET ENDPOINT */
   function reset_user_password(){
+    /* USER PASSWORD RESET ENDPOINT */
     register_rest_route('yka/v1', 'password_reset', array(
       'methods' => 'POST',
       'callback' => array( $this, 'reset_user_password_callback' ),
       'permission_callback' => '__return_true'
     )	);
+
+    /* USER PASSWORD VALIDATION ENDPOINT */
+    register_rest_route('yka/v1', 'user_password_validation', array(
+      'methods' => 'POST',
+      'callback' => array( $this, 'user_password_validation_callback' ),
+      'permission_callback' => function( $request ){
+        return current_user_can( 'edit_posts' );
+      }
+    )	);
+
   }
 
   function reset_user_password_callback( $request = null ){
@@ -21,7 +31,6 @@ class YKA_REST_PASSWORD_RESET extends YKA_REST_BASE{
   	$username	        = sanitize_text_field( $parameters['username'] );
     $new_password 	  = sanitize_text_field( $parameters['new_password'] );
     $current_user_obj = get_user_by('login', $username);
-
 
     $error = new WP_Error();
   	if ( empty( $username ) ) {
@@ -50,6 +59,35 @@ class YKA_REST_PASSWORD_RESET extends YKA_REST_BASE{
   	}
 
     return new WP_REST_Response( $response, 123 );
+
+  }
+
+  function user_password_validation_callback(  $request  ){
+
+    $response 	= array();
+    $error      = new WP_Error();
+    $user       = wp_get_current_user();
+    $password 	= sanitize_text_field( $request['password'] );
+
+    if ( empty( $password ) ) {
+  		$error->add( 'empty_password', __( "Password is required." ), array( 'status' => 400 ) );
+  		return $error;
+  	}
+
+    if ( ! wp_check_password( $password , $user->user_pass, $user->ID ) ) {
+      $error->add( 'incorrect_password', sprintf(
+        __( 'The password you entered for the username %s is incorrect.' ), $user->user_login ), array( 'status' => 400 )
+      );
+      return $error;
+    }
+
+    return new WP_REST_Response(
+      array(
+        'code'    => 'correct_password',
+        'message' => sprintf( __( 'The password you entered for the username %s is correct.' ), $user->user_login ),
+        'data'  => array( 'status'=>200 )
+      )
+    );
 
   }
 
